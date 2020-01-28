bb3406a021319d2c0ab94835b8ea09c2f64bf3c4
Budi Gunawan <budiindra.g@gmail.com>
2020-01-28 12:38:53 +0700

    merubah jika nama dan peran ada

--- ifelse.js
+++ ifelse.js
@@ -3,27 +3,26 @@ var nama = 'Budi';
 var peran = 'Penyihir';
 
 //jika nama kosong
-if (nama == '') {
+if (nama === '') {
     console.log('Nama harus diisi!');
 }
-else {
-    //jika nama ada dan peran kosong
-    if (peran == '') {
-        console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`);
-    }
-    //jika nama ada dan peran Ksatria
-    else if (peran == 'Ksatria') {
-        console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
+//jika nama ada dan peran kosong
+else if (nama !== '' && peran === '') {
+    console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`);
+}
+//jika nama dan peran ada
+else if (nama && peran) {
+    console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
+    //jika peran Ksatria
+    if (peran == 'Ksatria'){
         console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
     }
-    //jika nama ada dan peran Tabib
+    //jika peran Tabib
     else if (peran == 'Tabib') {
-        console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
         console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka.`);
     }
-    //jika nama ada dan peran Penyihir
+    //jika peran Penyihir
     else if (peran == 'Penyihir') {
-        console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
         console.log(`Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
     }
 }
