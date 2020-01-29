ae3ac10a1c1a6f2c199309dfa1a4dd15c5717f49
Budi Gunawan <budiindra.g@gmail.com>
2020-01-29 12:27:37 +0700

    satu for

--- latihannestedlooping.js
+++ latihannestedlooping.js
@@ -1,16 +1,34 @@
-for (var i = 0; i < 4; i++) {
-    var temp = '';
-    for (var j = 0; j <= i; j++) {
-       temp += '*';
-    }
-    console.log(temp);
-}
-//tes
+// for (var i = 0; i < 4; i++) {
+//     var temp = '';
+//     for (var j = 0; j <= i; j++) {
+//        temp += '*';
+//     }
+//     console.log(temp);
+// }
 
-for (var i = 3; i > 0; i--) {
-    var temp2 = '';
-    for (var j = 0; j < i; j++) {
-        temp2 += '*';
+// for (var i = 3; i > 0; i--) {
+//     var temp2 = '';
+//     for (var j = 0; j < i; j++) {
+//         temp2 += '*';
+//     }
+//     console.log(temp2);
+// }
+
+
+var n = 9;
+
+for (var i = 0; i < n; i++) {
+    var hasil = '';
+    if (i < Math.ceil(n/2)) {
+        for (var j = 0; j <= i; j++) {
+            hasil += '*';
+        }
     }
-    console.log(temp2);
+    else {
+        for (var k = n; k > i; k--)
+        {
+            hasil += '*';
+        }
+    }
+console.log(hasil);
 }
