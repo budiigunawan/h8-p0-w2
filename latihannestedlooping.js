c4683b4996807bb5ada773571cb84c27d5cf215f
Budi Gunawan <budiindra.g@gmail.com>
2020-01-29 10:24:08 +0700

    coba pyramid

---
+++ latihannestedlooping.js
@@ -0,0 +1,15 @@
+for (var i = 0; i < 4; i++) {
+    var temp1 = '';
+    for (var j = 0; j <= i; j++) {
+        temp1 += '*';
+    }
+    console.log(temp1);
+}
+
+for (var i = 3; i > 0; i--) {
+    var temp2 = '';
+    for (var j = 0; j < i; j++) {
+        temp2 += '*';
+    }
+    console.log(temp2);
+}
