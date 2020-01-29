41c96e0e72bc6309c28b1870b52d2914d063cc93
Budi Gunawan <budiindra.g@gmail.com>
2020-01-29 13:46:58 +0700

    membuat exercise 5 bermain dengan string

---
+++ string.js
@@ -0,0 +1,64 @@
+///soal pertama///
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+
+console.log(word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh);
+
+///soal kedua///
+console.log("");
+
+var word2 = 'Wow JavaScript is so cool';
+var exampleFirstWord = word2[0] + word2[1] + word2[2];
+var secondWord = word2[4] + word2[5] + word2[6] + word2[7] + word2[8] + word2[9] + word2[10] + word2[11] + word2[12] + word2[13];
+var thirdWord = word2[15] + word2[16];
+var fourthWord = word2[18] + word2[19];
+var fifthWord = word2[21] + word2[22] + word2[23] + word2[24];
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+
+///soal ketiga///
+console.log("");
+
+var word3 = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word3.substring(0, 3);
+var secondWord3 = word3.substring(4, 14);
+var thirdWord3 = word3.substring(15, 17);
+var fourthWord3 = word3.substring(18, 20);
+var fifthWord3 = word3.substring(21, 25);
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+
+///soal keempat///
+console.log("");
+
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word4.substring(0, 3);
+var secondWord4 = word4.substring(4, 14);
+var thirdWord4 = word4.substring(15, 17);
+var fourthWord4 = word4.substring(18, 20);
+var fifthWord4 = word4.substring(21, 25);
+
+var firstWordLength = exampleFirstWord4.length;
+var secondWordLength = secondWord4.length;
+var thirdWordLength = thirdWord4.length;
+var fourthWordLength = fourthWord4.length;
+var fifthWordLength = fifthWord4.length;
+
+console.log('First Word: ' + exampleFirstWord + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord + ', with length: ' + secondWordLength);
+console.log('Third Word: ' + thirdWord + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord + ', with length: ' + fifthWordLength);
